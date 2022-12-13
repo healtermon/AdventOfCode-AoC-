40f5f31bf846791059bb415be8b6f474148174ab
healtermon <healtermon@gmail.com>
2022-12-13 21:59:27 +0800

    fixed bug where elevation of ending position is calculated too high

--- 2022/12-hill-climbing-algorithm.py
+++ 2022/12-hill-climbing-algorithm.py
@@ -19,15 +19,14 @@ with open ("12-hill-climbing-algorithm.txt") as inputFile:
 
 def height(c):
     if ord(c) in range(ord('a'),ord('z')+1): return ord(c)-ord('a')
-    elif c == 'S': return 0
-    elif c == 'E': return ord('z')
+    elif c == 'S': return ord('a')-ord('a')
+    elif c == 'E': return ord('z')-ord('a')
     else: raise Exception("wtf I missed something?")
 
 b = [list(x) for x in a]
 c = [[height(y) for y in x] for x in b]
 d = pd.DataFrame(c)
-sns.heatmap(data=d,
-            cmap='RdBu_r')
+sns.heatmap(data=d, cmap='RdBu_r')
 plt.show()                      # show visualisation, the actual code starts below
 
 def findChar(df:pd.DataFrame, ch) -> Tuple:
@@ -46,7 +45,7 @@ end   = findEnd  (e)
 
 
 def inPlayfield(df, p:Tuple): return p[0] in range(0,df.shape[R]) and p[1] in range(0,df.shape[C])
-def scalable(df,c,n): return height(df.iloc[n[R],n[C]]) - height(df.iloc[c[R],c[C]]) <= 1
+def scalable(df,c:Tuple,n:Tuple): return height(df.iloc[n[R],n[C]]) - height(df.iloc[c[R],c[C]]) <= 1
 def ok(df,c,n): return inPlayfield(df,n) and scalable(df,c,n)
 
 visited = []
@@ -61,7 +60,7 @@ def adjacent(n):
 def posList(l:list): return [posNode(n) for n in l]
 def posNode(n:Tuple): return (n[R],n[C])
 
-def unrecorded(v,e,l): return [n for n in l if not (posNode(n) in posList(v) or posNode(n) in posList(e))]
+def unrecorded(v:list,e:list,l:list): return [n for n in l if not (posNode(n) in posList(v) or posNode(n) in posList(e))]
 def traversable(df,c,l): return [n for n in l if ok(df,(c[R],c[C]),(n[R],n[C]))]
 
 def popMin(e:list):
@@ -73,7 +72,9 @@ def popMin(e:list):
 while edges:
     curr = popMin(edges)
     visited.append(curr)
-    edges.extend(traversable(e,curr,unrecorded(visited,edges,adjacent(curr))))
+    g = traversable(e,curr,unrecorded(visited,edges,adjacent(curr)))
+    print(g)
+    edges.extend(g)
 
 def makeDF(df,ps):
     x = np.zeros(df.shape, dtype=int)
@@ -82,8 +83,7 @@ def makeDF(df,ps):
     return pd.DataFrame(x)
 
 f = makeDF(e, visited)
-sns.heatmap(data=f,
-            cmap='RdBu_r')
+sns.heatmap(data=f, cmap='RdBu_r')
 plt.show()
 
 def getPos(r,c,ps):
